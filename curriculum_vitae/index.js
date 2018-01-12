d797c741b768a3af2b167880f0c505fb4e077209
hastorga <hm.astorga@gmail.com>
2018-01-12 11:35:34 -0300

    Se agrega el curriculum para trabajar en API REST

---
+++ curriculum_vitae/index.js
@@ -0,0 +1,76 @@
+'use srict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// realizando conexion con la bd
+mongoose.connect('mongodb://localhost/peliculas', { useMongoClient: true });
+
+var peliculaSchemaJSON = {
+    titulo: String
+ 
+};
+
+// Creando Schema
+var peliculaSchema = new Schema(peliculaSchemaJSON);
+
+// Creando modelo
+var Pelicula = mongoose.model("peliculas",peliculaSchema);
+
+
+app.get("/list",(req,res) => {
+
+    Pelicula.find((err,result) => {
+
+        console.log(result);
+        res.send(result);
+
+      });
+    });
+
+
+
+//app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/hola', (req, res) => res.send('Hola! Bienvenido'));
+
+app.get('/contacto', (req, res) => res.send('Contactame a hm.astorga@gmail.com'));
+
+app.listen(80, () => console.log('Example app listening on port 80!'));
+
+
+// conexion 
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  database: "workers",
+  host: "localhost",
+  user: "root",
+  password: "barracuda132"
+});
+
+app.get("/bd",function(req,res){
+    con.query('SELECT * from worker', function(err, rows, fields) {
+    con.end();
+      if (!err){
+        console.log('The solution is: ', rows),
+        res.send(rows);
+      }else
+        console.log('Error while performing Query.');
+      });
+    });
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.use(express.static('assets'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
