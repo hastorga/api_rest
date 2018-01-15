a3e656867cdb790435364ae7d751561dbb3662f0
hastorga <hm.astorga@gmail.com>
2018-01-15 10:57:07 -0300

    cambio en ruta

--- curriculum_vitae/index.js
+++ curriculum_vitae/index.js
@@ -166,4 +166,5 @@ con.connect(function(err) {
   console.log("Connected!");
 });
 
+app.use(express.static('curriculum_vitae'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
