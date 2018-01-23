55f3c815dd3a8db57c02fa8df1782266a1d40ba6
Héctor Astorga <hm.astorga@gmail.com>
2018-01-22 22:21:58 -0300

    Se adapta el codigo para que se trabaje de forma local
    Se elimina la conexión a la BD MySQL y se adapta para trabajar con MongoDB en localhost:3000

--- curriculum_vitae/index.js
+++ curriculum_vitae/index.js
@@ -134,37 +134,45 @@ app.get('/hola', (req, res) => res.send('Hola! Bienvenido'));
 
 app.get('/contacto', (req, res) => res.send('Contactame a hm.astorga@gmail.com'));
 
-app.listen(80, () => console.log('Example app listening on port 80!'));
+// PARA EL SERVIDOR!
+//app.listen(80, () => console.log('Example app listening on port 80!'));
 
+//LOCAL
+var http = require('http');
+var server = http.createServer(app);
+server.listen(3000, 'localhost');
+server.on('listening', function() {
+    console.log('Express server started on port %s at %s', server.address().port, server.address().address);
+});
 
 // conexion 
 
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  database: "workers",
-  host: "localhost",
-  user: "root",
-  password: "barracuda132"
-});
+// var con = mysql.createConnection({
+//  database: "workers",
+//  host: "localhost",
+//  user: "root",
+//  password: "barracuda132"
+// });
 
-// MYSQL
+// // MYSQL
 
-app.get("/bd",function(req,res){
-    con.query('SELECT * from worker', function(err, rows, fields) {
-    con.end();
-      if (!err){
-        console.log('The solution is: ', rows),
-        res.send(rows);
-      }else
-        console.log('Error while performing Query.');
-      });
-    });
+// app.get("/bd",function(req,res){
+//     con.query('SELECT * from worker', function(err, rows, fields) {
+//     con.end();
+//       if (!err){
+//         console.log('The solution is: ', rows),
+//         res.send(rows);
+//       }else
+//         console.log('Error while performing Query.');
+//       });
+//     });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
 
 app.use(express.static('assets'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/assets/index.html'));
